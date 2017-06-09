32cdef05143343ab2922cc52984c7fb1c72abad4
Christian Nagel <christian@christiannagel.com>
2017-06-09 21:10:46 +0200

    functional programming

---
+++ FunctionalProgramming/LocalFunctions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LocalFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
