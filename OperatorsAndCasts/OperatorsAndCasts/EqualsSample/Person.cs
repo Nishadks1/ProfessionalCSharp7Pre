b5d54ca19d6ec4ed0c52adf2b0eb2877105ca7f5
Christian Nagel <christian@christiannagel.com>
2017-06-19 23:44:33 +0200

    more samples for operators and casts

---
+++ OperatorsAndCasts/OperatorsAndCasts/EqualsSample/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace EqualsSample
+{
+    class Person : IEquatable<Person>
+    {
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Person other)
+            {
+                return Equals(other);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Equals(Person other)
+        {
+            if (other == null) return false;
+            return FirstName == other.FirstName && LastName == other.LastName;
+        }
+        public override int GetHashCode() => FirstName.GetHashCode() ^ LastName.GetHashCode();
+
+        public static bool operator ==(Person p1, Person p2) => p1.Equals(p2);
+        public static bool operator !=(Person p1, Person p2) => !p1.Equals(p2);
+
+    }
+}
---
+++ OperatorsAndCasts/OperatorsAndCasts/EqualsSample/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EqualsSample
+{
+    class SomeClass { }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReferenceEqualsSample();
+            OverrideEqualsSample();
+        }
+
+        private static void OverrideEqualsSample()
+        {
+            Person p1 = new Person("Stephanie", "Nagel");
+            Person p2 = new Person("Stephanie", "Nagel");
+
+            bool b1 = p1 == p2;
+            Console.WriteLine($"p1 == p2 returns {b1}");
+
+            bool b2 = object.ReferenceEquals(p1, p2);
+            Console.WriteLine($"ReferenceEquals returns {b2}");
+
+            bool b3 = p1.Equals(p2);
+            Console.WriteLine($"overridden Equals returns {b3}");
+
+            
+            bool b4 = EqualityComparer<Person>.Default.Equals(p1, p2);
+            Console.WriteLine($"EqualityComparer returns {b4}");
+        }
+
+        static void ReferenceEqualsSample()
+        {
+            SomeClass x = new SomeClass(), y = new SomeClass(), z = x;
+            bool b1 = object.ReferenceEquals(null, null);  // returns true
+            Console.WriteLine(b1);
+            bool b2 = object.ReferenceEquals(null, x);     // returns false
+            Console.WriteLine(b2);
+            bool b3 = object.ReferenceEquals(x, y);        // returns false because x and y
+                                                           // references different objects
+            Console.WriteLine(b3);
+            bool b4 = object.ReferenceEquals(x, z);        // returns true because x and z
+                                                           // references the same object
+
+
+            Console.WriteLine(b4);
+        }
+    }
+}
